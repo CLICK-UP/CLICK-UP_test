5f263f07d05977647d969d44c183c870fd07d395
wangjunxiao <wangjunxiao@live.com>
2018-08-08 13:59:51 +0800

    fix gre config

--- client/gre.sh
+++ client/gre.sh
@@ -2,10 +2,12 @@ ovs-vsctl del-br vfunc-br
 ovs-vsctl add-br vfunc-br
 ovs-vsctl add-port vfunc-br p1 -- set interface p1 ofport_request=1
 ovs-vsctl set interface p1 type=internal
-ifconfig p1 "IPAddress/netmask" up
+ifconfig p1 10.0.0.1/24
+ifconfig p1 hw ether fa:fe:ca:5d:97:6c
+ifconfig p1 up
 ifconfig p1 promisc
 ifconfig p1 mtu 1400
-ovs-vsctl add-port vfunc-br vfunc-forward -- set interface vfunc-forward type=gre options:df_default=true options:in_key=flow options:local_ip=30.0.0.2 options:out_key=flow options:remote_ip=30.0.0.1
+ovs-vsctl add-port vfunc-br vfunc-forward -- set interface vfunc-forward type=gre options:df_default=true options:in_key=flow options:local_ip=192.168.1.41 options:out_key=flow options:remote_ip=192.168.1.13
 ovs-vsctl set interface vfunc-forward ofport_request=2
 ovs-ofctl add-flow vfunc-br "priority=10,in_port=1,actions=output:2"
 ovs-ofctl add-flow vfunc-br "priority=10,in_port=2,actions=output:1"
--- rpi/gre.sh
+++ rpi/gre.sh
@@ -8,8 +8,8 @@ ifconfig p1 promisc
 ifconfig p2 promisc
 ifconfig p1 mtu 1400
 ifconfig p2 mtu 1400
-ovs-vsctl add-port clickup clickup-forward -- set interface clickup-forward type=gre options:in_key=flow options:local_ip=192.168.0.13 options:out_key=flow options:remote_ip=192.168.0.41
-ovs-vsctl add-port clickup clickup-backward -- set interface clickup-backward type=gre options:in_key=flow options:local_ip=192.168.0.13 options:out_key=flow options:remote_ip=192.168.0.42
+ovs-vsctl add-port clickup clickup-forward -- set interface clickup-forward type=gre options:in_key=flow options:local_ip=192.168.1.13 options:out_key=flow options:remote_ip=192.168.1.41
+ovs-vsctl add-port clickup clickup-backward -- set interface clickup-backward type=gre options:in_key=flow options:local_ip=192.168.1.13 options:out_key=flow options:remote_ip=192.168.1.42
 ovs-vsctl set interface clickup-forward ofport_request=3
 ovs-vsctl set interface clickup-backward ofport_request=4
 ovs-ofctl add-flow clickup "priority=10,in_port=3,actions=output:1"
